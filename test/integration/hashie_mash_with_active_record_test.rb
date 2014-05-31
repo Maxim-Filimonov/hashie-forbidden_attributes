f982d3d70f401631fb7ef4361be76ce661256eb9
Maxim Filimonov <tpaktopsp@gmail.com>
2014-05-31 13:30:42 +1000

    Add integration test

---
+++ test/integration/hashie_mash_with_active_record_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class HashieMashWithActiveRecordTest < ActionDispatch::IntegrationTest
+  test "bird can be created with mash" do
+    assert_difference("Bird.count", +1) do
+      post "/birds", name: 'Birdie'
+    end
+  end
+end
